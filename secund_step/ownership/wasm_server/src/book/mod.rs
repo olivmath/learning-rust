cde90eb4a83f70a5327d44bf34a6e59966caef53
Lucas Oliveira <olivmath@protonmail.com>
2023-03-12 16:55:57 -0300

    update: book to wasm

--- secund_step/ownership/wasm_server/src/book/mod.rs
+++ secund_step/ownership/wasm_server/src/book/mod.rs
@@ -1,83 +1,82 @@
 use lazy_static::lazy_static;
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 use std::{
     collections::HashMap,
     sync::{Arc, Mutex},
 };
-use uuid::Uuid;
 
 lazy_static! {
-    static ref DB: Arc<Mutex<HashMap<String, Book>>> = Arc::new(Mutex::new(HashMap::new()));
+    static ref DB: Arc<Mutex<HashMap<String, Wasm>>> = Arc::new(Mutex::new(HashMap::new()));
 }
 
-#[derive(Deserialize, Serialize, PartialEq, Eq, Debug, Clone)]
-pub struct Book {
-    pub title: String,
-    pub year: u16,
+#[derive(Debug, Clone, Deserialize)]
+pub struct Wasm {
+    pub name: String,
+    pub id: String,
+    pub file: Vec<u8>,
+    pub size: usize,
+    pub main_function: String,
 }
 
-impl Book {
-    fn new(title: &str, year: u16) -> Self {
-        Book {
-            title: String::from(title),
-            year,
+impl Wasm {
+    pub fn new(name: String, id: String, file: Vec<u8>, main_function: String) -> Wasm {
+        Wasm {
+            name,
+            id,
+            file,
+            size: file.len(),
+            main_function,
         }
     }
-    pub fn default() -> Self {
-        Book {
-            title: "".into(),
-            year: 0,
-        }
-    }
 }
 
-impl std::fmt::Display for Book {
+impl std::fmt::Display for Wasm {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{} ({})", self.title, self.year)
+        write!(f, "{} ({}) {}", self.id, self.main_function, self.size)
     }
 }
 
-pub struct Library {
-    books: Arc<Mutex<HashMap<String, Book>>>,
+pub struct Storage {
+    wasms: Arc<Mutex<HashMap<String, Wasm>>>,
 }
 
-impl Library {
-    pub fn new() -> Library {
-        Library {
-            books: Arc::clone(&DB),
+impl Storage {
+    pub fn new() -> Storage {
+        Storage {
+            wasms: Arc::clone(&DB),
         }
     }
 
     pub fn len(&self) -> usize {
-        self.books.lock().unwrap().len()
+        self.wasms.lock().unwrap().len()
     }
 
     pub fn is_empty(&self) -> bool {
-        self.books.lock().unwrap().is_empty()
+        self.wasms.lock().unwrap().is_empty()
     }
 
-    pub fn add_book(&mut self, id: String, book: Book) -> Option<Book> {
-        self.books.lock().unwrap().insert(id, book)
+    pub fn add_wasm(&mut self, wasm: Wasm) -> Option<Wasm> {
+        self.wasms.lock().unwrap().insert(wasm.id, wasm)
     }
 
-    pub fn modify_book(&mut self, id: String, book: Book) -> Option<Book> {
-        self.books.lock().unwrap().insert(id, book)
+    pub fn modify_wasm(&mut self, wasm: Wasm) -> Option<Wasm> {
+        self.wasms.lock().unwrap().insert(wasm.id, wasm)
     }
 
-    pub fn remove_book(&mut self, id: String) -> Option<Book> {
-        self.books.lock().unwrap().remove(&id)
+    pub fn remove_wasm(&mut self, id: String) -> Option<Wasm> {
+        self.wasms.lock().unwrap().remove(&id)
     }
 
-    pub fn all_books(&self) -> Vec<Book> {
-        self.books
+    pub fn all_wasms(&self) -> Vec<Wasm> {
+        self.wasms
             .lock()
             .unwrap()
             .iter()
             .map(|(_, v)| v.clone())
-            .collect::<Vec<Book>>()
+            .collect::<Vec<Wasm>>()
     }
 
-    pub fn get_book(&self, id: String) -> Option<Book> {
-        self.books.lock().unwrap().get(&id).cloned()
+    pub fn get_wasm(&self, id: String) -> Option<Wasm> {
+        self.wasms.lock().unwrap().get(&id).cloned()
     }
 }
